baabe8c20e39055081112c8345371fd54007a95a
LANNDS18 <LANNDS18@icloud.com>
2024-04-30 16:27:01 +0100

    bugfix/ 1. Fixing the generation args 'n' and 'logit_bias' initialization error 2. Implementing additional layer of type and value checking for self.generate_args 3. Implementing automatic gpt model checking by calling openai api 4. Inserting additional docstring for mock api client and test client

--- xyz/utils/test_tool/mock_openai.py
+++ xyz/utils/test_tool/mock_openai.py
@@ -1,30 +1,119 @@
+"""
+
+==============
+ Mock OpenAI API Client
+==============
+@file_name: mock_openai.py
+@description: This module contains a mock implementation of the OpenAI API designed to simulate responses for chat completions.
+The MockOpenAI class within this module can be used to develop and test applications that integrate with OpenAI's chat completions
+without the need to make actual API calls. This helps in scenarios where API usage might be limited or costly, and during the
+initial phases of development where frequent interaction with the OpenAI API is required.
+
+## Initialization
+The MockOpenAI class requires an API key upon initialization, which is simulated within this mock setup.
+It initializes with default responses and configurations, which can be customized based on specific testing needs.
+
+For detailed information on these parameters, refer to the OpenAI documentation. https://platform.openai.com/docs/api-reference/chat/create
+
+## Methods
+The class includes two primary methods for interacting with the simulated OpenAI:
+
+- `create`: Simulates the creation of chat completions, supporting both standard and streaming outputs.
+  Raises ValueError if the provided parameters do not meet expected formats or values.
+
+- `stream_create`: Simulates the streaming of chat completions for testing streaming functionalities.
+
+These methods simplify the process of integrating OpenAI functionalities into your applications, allowing for both
+standard and streaming interactions.
+
+## Motivation
+The main motivation behind creating this mock class is to provide developers with a reliable and easy-to-use tool for
+testing and developing applications that interact with the OpenAI chat completion API. It ensures that developers
+can prototype and debug features without incurring API costs or hitting rate limits, fostering a smoother development
+experience.
+
+"""
+
+
 from collections.abc import Mapping, Sequence
 from typing import Final, NamedTuple, Optional
 
-_DEFAULT_REPLY: Final[str] = 'RESPONSE:Hello'
+_DEFAULT_REPLY: Final[str] = 'RESPONSE:Hello\n'
 _DEFAULT_SCORE: Final[float] = -0.1
-_DEFAULT_STREAM_REPLY: Final[str] = 'STREAM_RESPONSE:HELLO'
+_DEFAULT_STREAM_REPLY: Final[str] = 'STREAM_RESPONSE:HELLO\n'
 
 
 class OpenAIMessage(NamedTuple):
+    """
+    Represents a message within the chat completion structure.
+
+    Attributes
+    ----------
+    role : str
+        The role of the entity sending the message (e.g., 'user', 'assistant').
+    content : str
+        The content of the message.
+    """
     role: str
     content: str
 
 
 class ChatCompletionTokenLogprob(NamedTuple):
+    """
+    Represents a single token and its associated log probability within a message.
+
+    Attributes
+    ----------
+    token : str
+        The token (word or punctuation) that was part of the response.
+    logprob : float
+        The log probability of the token, indicating how likely it was to occur.
+    """
     token: str
     logprob: float
 
 
 class ChoiceLogProbs(NamedTuple):
+    """
+    Contains a sequence of `ChatCompletionTokenLogprob` representing the tokens and their log probabilities for a choice.
+
+    Attributes
+    ----------
+    content : Sequence[ChatCompletionTokenLogprob]
+        A sequence of token log probability pairs for a particular completion.
+    """
     content: Sequence[ChatCompletionTokenLogprob]
 
 
 class Delta(NamedTuple):
+    """
+    Optional additional information about the change between two messages or states.
+
+    Attributes
+    ----------
+    content : Optional[str]
+        Textual content describing or quantifying the change, if any.
+    """
     content: Optional[str] = None
 
 
 class Choice(NamedTuple):
+    """
+    Represents a single choice or option provided by the completion system.
+
+    Attributes
+    ----------
+    index : int
+        The index of the choice among all options provided in the completion.
+    message : OpenAIMessage
+        The message associated with this choice.
+    logprobs : ChoiceLogProbs
+        Log probabilities for tokens in the message.
+    finish_reason : str
+        The reason why this part of the conversation or prompt was concluded.
+    delta : Delta
+        Optional additional data or context relevant to this choice.
+    """
     index: int
     message: OpenAIMessage
     logprobs: ChoiceLogProbs
@@ -33,15 +122,57 @@ class Choice(NamedTuple):
 
 
 class ChatCompletion(NamedTuple):
+    """
+    Encapsulates the overall completion response from a chat model.
+
+    Attributes
+    ----------
+    choices : Sequence[Choice]
+        A sequence of choices provided by the model. Each choice includes detailed information such as the message and associated probabilities.
+    """
     choices: Sequence[Choice]
 
 
 class MockOpenAI:
-    """A mock of the OpenAI class that provides a chat.completions.create method, now with streaming support."""
+    """
+    A mock class that simulates the behavior of the OpenAI API, supporting both standard and streaming chat completion features.
 
-    stream: bool
+    This class is intended for testing and development purposes, allowing users to simulate responses from the OpenAI chat completion API without actual API calls.
+
+    Attributes
+    ----------
+    api_key : str
+        The API key used for authentication (simulated in this mock).
+
+    Parameters
+    ----------
+    api_key : str
+        A string representing a fake API key for the OpenAI API.
+
+    Methods
+    -------
+    chat()
+        Provides access to chat interface methods.
+
+    completions()
+        Provides access to the completion interface methods.
+
+    create(model, messages, stream=False, **kwargs)
+        Simulates the creation of chat completions, either in standard or stream mode.
+
+    stream_create(choices)
+        Simulates the creation of streamed chat completions.
+    """
 
     def __init__(self, api_key: str):
+        """
+        Initializes the MockOpenAI instance with the provided API key.
+
+        Parameters
+        ----------
+        api_key : str
+            A string representing a fake API key for the OpenAI API.
+        """
         self.api_key = api_key
         self._reply = _DEFAULT_REPLY
         self._stream_reply = _DEFAULT_STREAM_REPLY
@@ -50,16 +181,27 @@ class MockOpenAI:
 
     @property
     def chat(self):
+        """
+        Simulated chat interface property.
+
+        Returns
+        -------
+        self
+            Returns the instance itself, simulating the chat interface.
+        """
         return self
 
     @property
     def completions(self):
-        return self
+        """
+        Simulated completions interface property.
 
-    def stream_create(self, choices):
-        if self.stream:
-            yield ChatCompletion(choices=choices)
-            return None
+        Returns
+        -------
+        self
+            Returns the instance itself, simulating the completions interface.
+        """
+        return self
 
     def create(
             self,
@@ -68,6 +210,25 @@ class MockOpenAI:
             stream: bool = False,
             **kwargs,
     ) -> list[ChatCompletion] | ChatCompletion:
+        """
+        Simulates the creation of chat completions based on input parameters, supporting both standard and streaming outputs.
+
+        Parameters
+        ----------
+        model : str
+            The model identifier, which in real use would determine the behavior of the completion.
+        messages : list of dict
+            A sequence of dictionaries representing messages in the conversation.
+        stream : bool, optional
+            If True, simulates streaming completions.
+        **kwargs : dict
+            Additional keyword arguments for simulating API parameters such as 'n' for the number of completions.
+
+        Returns
+        -------
+        list of ChatCompletion or ChatCompletion
+            Depending on the 'stream' parameter, returns a single ChatCompletion or a list of ChatCompletion objects.
+        """
         del model, messages
         samples = 1
         if 'n' in kwargs:
@@ -89,7 +250,6 @@ class MockOpenAI:
             )
             for i in range(samples)
         ]
-        self.stream = stream
         if stream:
             self._delta_ptr += 1
             return [ChatCompletion(choices=choices)]
--- xyz/utils/test_tool/test_client.py
+++ xyz/utils/test_tool/test_client.py
@@ -4,25 +4,67 @@ TestClient
 ============
 @file_name: test_client.py
 @description:
-This is a dummy LLM-CLient. It will not do any request. But it can check the request and response for openai API.
+This is an openai TestClient. It will not do any request. But it can check the request and response for openai API.
 
 ## Initialization
 Users initialize the OpenAIClient by specifying the api_key and generate_args:
 
 - `api_key`: The API key for OpenAI. This must be obtained from your OpenAI account.
-- `generate_args`: Arguments for the chat completion request. For detailed information on these parameters, refer to the
- OpenAI documentation. https://platform.openai.com/docs/api-reference/chat/create
+- `generate_args`: Default parameters for generating chat completions.
+    The 'generate_args' attribute supports the following keys and types:
+        - 'model': str,
+            the model identifier, e.g., 'gpt-4-turbo'.
+        - 'frequency_penalty': Optional[float]
+            adjusts frequency of token usage to prevent repetition.
+        - 'temperature': Optional[float]
+            controls randomness in the response generation.
+        - 'top_p': Optional[float]
+            sets the nucleus sampling rate defining the probability mass used in token selection.
+        - 'max_tokens': Optional[int]
+            the maximum number of tokens to generate.
+        - 'n': Optional[int]
+            number of completions to generate for each prompt.
+        - 'logprobs': Optional[bool]
+            whether to return the log probabilities over the set of specified tokens.
+        - 'presence_penalty': Optional[float]
+            adjusts the likelihood of tokens based on their prior presence in the text.
+        - 'seed': Optional[int]
+            seed for random number generator for reproducibility.
+        - 'top_logprobs': Optional[int]
+            number of most likely tokens and their log probabilities to return.
+        - 'time_out': float | httpx.Timeout | None
+            the maximum duration to wait for the server response.
+        - 'logit_bias': dict | None
+            specifies biases for or against specific tokens during generation.
+        - 'user': Optional[str]
+            identifier for the user in the session.
+        - 'stop': Optional[str] | List[str]
+            tokens that signal the end of a generation.
+
+For detailed information on these parameters, refer to the OpenAI documentation. https://platform.openai.com/docs/api-reference/chat/create
 
 ## Methods
 The class includes two primary methods for interacting with OpenAI:
+
+- `validate_arguments`: This function is used to validate the customized generation parameters.
+Raises:
+    - ValueError: If any generation arguments are invalid or of incorrect type.
+
+- `get_gpt_models`:  Retrieves a list of available GPT models from the OpenAI API.
+Raises:
+    - If there is an error in communicating with the OpenAI API, this method will catch the OpenAIError and provide a pre-defined list of GPT model identifiers.
 
 - `run`: This method is used to make standard requests to OpenAI. It accepts messages, images, and optionally tools as
 parameters:
     - `messages`: A list of strings, each representing a conversation turn.
     - `images`: A list of URLs pointing to images to be included in the request.
     - `tools`: An optional list that specifies additional tools to be used in the request.
+
 - `stream_run`: This method is designed for streaming requests to OpenAI and also requires the messages and images
-parameters.
+parameters:
+    - `messages`: A list of strings, each representing a conversation turn.
+    - `tools`: An optional list that specifies additional tools to be used in the request.
+
 These methods simplify the process of integrating OpenAI functionalities into your applications, allowing for both
 standard and streaming interactions.
 
@@ -36,14 +78,13 @@ import os
 import re
 import httpx
 import traceback
-from typing import Generator, Union, Optional, Dict, List
+from typing import Generator, Optional, Dict, List
 
 from dotenv import load_dotenv
 from openai import OpenAI
 from openai import OpenAIError
 from openai import Stream
 from openai.types.chat import ChatCompletion, ChatCompletionChunk
-from typing_extensions import Literal
 
 from mock_openai import MockOpenAI
 
@@ -52,22 +93,68 @@ __all__ = ["TestClient"]
 
 class TestClient:
     """
-    The OpenAI client which uses the OpenAI API to generate responses to messages.
+    A mock client to simulate interactions with the OpenAI API without making actual network requests.
+    It allows for testing and validation of request and response handling, ensuring that integrations
+    are correctly set up to use OpenAI functionalities.
+
+    Attributes
+    ----------
+        client : OpenAI
+            The OpenAI client instance used for API interactions.
+        generate_args : dict
+            Default parameters for generating chat completions.
+        last_time_price : float
+            Placeholder for tracking pricing, not implemented.
     """
     client: OpenAI
     generate_args: dict
     last_time_price: float
 
     def __init__(self, api_key=None, **generate_args):
-        """Initializes the OpenAI Client.
+        """
+        A mock client to simulate interactions with the OpenAI API without making actual network requests.
+        It allows for testing and validation of request and response handling, ensuring that integrations
+        are correctly set up to use OpenAI functionalities.
 
         Parameters
         ----------
         api_key : str, optional
             The OpenAI API key.
+
         generate_args : dict, optional
-            Arguments for the chat completion request.
+            Default parameters for generating chat completions.
+            The 'generate_args' attribute supports the following keys and types:
+            - 'model': str,
+                the model identifier, e.g., 'gpt-4-turbo'.
+            - 'frequency_penalty': Optional[float]
+                adjusts frequency of token usage to prevent repetition.
+            - 'temperature': Optional[float]
+                controls randomness in the response generation.
+            - 'top_p': Optional[float]
+                sets the nucleus sampling rate defining the probability mass used in token selection.
+            - 'max_tokens': Optional[int]
+                the maximum number of tokens to generate.
+            - 'n': Optional[int]
+                number of completions to generate for each prompt.
+            - 'logprobs': Optional[bool]
+                whether to return the log probabilities over the set of specified tokens.
+            - 'presence_penalty': Optional[float]
+                adjusts the likelihood of tokens based on their prior presence in the text.
+            - 'seed': Optional[int]
+                seed for random number generator for reproducibility.
+            - 'top_logprobs': Optional[int]
+                number of most likely tokens and their log probabilities to return.
+            - 'time_out': float | httpx.Timeout | None
+                the maximum duration to wait for the server response.
+            - 'logit_bias': dict | None
+                specifies biases for or against specific tokens during generation.
+            - 'user': Optional[str]
+                identifier for the user in the session.
+            - 'stop': Optional[str] | List[str]
+                tokens that signal the end of a generation.
+
             ref: https://platform.openai.com/docs/api-reference/chat/create
+
         """
 
         try:
@@ -78,95 +165,110 @@ class TestClient:
         except OpenAIError:
             raise OpenAIError("The OpenAI client is not available. Please check the OpenAI API key.")
 
-        # Set the default generate arguments for OpenAI's chat completions
         self.generate_args = {
             "model": "gpt-4-turbo",
             "temperature": 0.,
             "top_p": 1.0
         }
-        # If the user provides generate arguments, update the default values
+
         self.generate_args.update(generate_args)
-        self.validate_arguments()
         self.mock_openai = MockOpenAI(api_key)
 
     def validate_arguments(self):
         """
-        Validate the generation argument in openai chat
+        Validates the generation arguments against expected types and values,
+        ensuring all provided arguments meet API requirements.
 
         Raises
         ------
         ValueError
-            If the generation argument is invalid.
+           If any generation arguments are invalid or of incorrect type.
         """
+
         valid_keys = {
-            'model': Union[
-                str,
-                Literal[
-                    "gpt-4-0125-preview",
-                    "gpt-4-turbo-preview",
-                    "gpt-4-1106-preview",
-                    "gpt-4-vision-preview",
-                    "gpt-4",
-                    "gpt-4-0314",
-                    "gpt-4-0613",
-                    "gpt-4-32k",
-                    "gpt-4-32k-0314",
-                    "gpt-4-32k-0613",
-                    "gpt-3.5-turbo",
-                    "gpt-3.5-turbo-16k",
-                    "gpt-3.5-turbo-0301",
-                    "gpt-3.5-turbo-0613",
-                    "gpt-3.5-turbo-1106",
-                    "gpt-3.5-turbo-0125",
-                    "gpt-3.5-turbo-16k-0613",
-                ],
-            ],
-            "frequency_penalty": Optional[float],
-            'temperature': Optional[float],
-            'top_p': Optional[float],
-            "max_tokens": Optional[int],
-            'n': Optional[int],
-            "logprobs": Optional[bool],
-            'presence_penalty': Optional[float],
-            'seed': Optional[int],
-            'top_logprobs': Optional[int],
-            'time_out': float | httpx.Timeout | None,
-            "logit_bias": dict | None,
-            'user': Optional[str],
-            'stop': Optional[str] | List[str],
-            # "functions":  list | None | NotGiven,
-            # "function_call": completion_create_params.FunctionCall | None | NotGiven,
-            # 'response_format': completion_create_params.ResponseFormat | None | NotGiven,
-            # 'tool_choice': ChatCompletionToolChoiceOptionParam | None | NotGiven,
+            'model': (
+                str, lambda x: (x in self.get_gpt_models(), f"is not a recognized model in {self.get_gpt_models()}")),
+            "frequency_penalty": (Optional[float], lambda x: (-2.0 <= x <= 2.0, "must be between -2 and 2")),
+            'temperature': (Optional[float], lambda x: (0.0 <= x <= 1.0, "must be between 0 and 1")),
+            'top_p': (Optional[float], lambda x: (0.0 <= x <= 1.0, "must be between 0 and 1")),
+            "max_tokens": (Optional[int], None),
+            'n': (Optional[int], lambda x: (x >= 1, "must be at least 1")),
+            "logprobs": (Optional[bool], None),
+            'presence_penalty': (Optional[float], lambda x: (-2.0 <= x <= 2.0, "must be between -2 and 2")),
+            'seed': (Optional[int], None),
+            'top_logprobs': (Optional[int], None),
+            'time_out': (float | httpx.Timeout | None, None),
+            "logit_bias": (dict | None, lambda d: (
+                all(isinstance(k, str) and isinstance(v, int) and -100 <= v <= 100 for k, v in d.items()),
+                "keys must be strings and values must be integers, associated bias value must be from -100 to 100")),
+            'user': (Optional[str], None),
+            'stop': (Optional[str] | List[str], None),
         }
 
         errors = []
-
         for key, value in self.generate_args.items():
             if key not in valid_keys:
                 errors.append(f"Invalid argument: {key}")
-            elif key == 'logit_bias':
-                if not isinstance(value, dict):
-                    errors.append(
-                        f"Invalid type for {key}: Expected {dict}, got {type(value).__name__}")
-                else:
-                    if key == "logit_bias":
-                        if not all(isinstance(k, str) for k in value.keys()) or not all(
-                                isinstance(v, int) for v in value.values()):
-                            errors.append(f"Invalid type in dictionary for {key}: Expected str keys and int values")
-            else:
-                expected_type = valid_keys[key]
-                if not isinstance(value, expected_type):
-                    errors.append(
-                        f"Invalid type for {key}: Expected {expected_type}, got {type(value).__name__}")
+                continue
+
+            expected_type, validator = valid_keys[key]
+            if not isinstance(value, expected_type):
+                expected_type_name = expected_type.__name__ if not isinstance(expected_type, tuple) else ', '.join(
+                    t.__name__ for t in expected_type)
+                errors.append(f"Invalid type for {key}: Expected {expected_type_name}, got {type(value).__name__}")
+
+            if value is not None and validator:
+                if callable(validator):
+                    is_valid, error_msg = validator(value)
+                    if not is_valid:
+                        errors.append(f"Invalid value for '{key}': {value}. Value {error_msg}")
 
         if errors:
             error_message = "Argument validation errors:\n" + "\n".join(errors)
             raise ValueError(error_message)
 
-        for key, value in valid_keys.items():
-            if key not in self.generate_args:
-                self.generate_args[key] = None
+    def get_gpt_models(self):
+        """
+        Retrieves a list of available GPT models from the OpenAI API.
+
+        This method queries the OpenAI API to get a list of models that include
+        'gpt' in their identifier. If the API call fails, a predefined list of
+        GPT models is returned as a fallback.
+
+        Returns
+        -------
+        list of str
+            A list of model identifiers that include 'gpt'.
+
+        Raises
+        ------
+        OpenAIError
+            If there is an error in communicating with the OpenAI API, this
+            method will catch the OpenAIError and provide a pre-defined list of GPT
+            model identifiers.
+        """
+        try:
+            models = self.client.models.list()
+            openai_api_models = [m.id for m in models if 'gpt' in m.id]
+        except OpenAIError:
+            openai_api_models = ["gpt-4-0125-preview",
+                                 "gpt-4-turbo-preview",
+                                 "gpt-4-1106-preview",
+                                 "gpt-4-vision-preview",
+                                 "gpt-4",
+                                 "gpt-4-0314",
+                                 "gpt-4-0613",
+                                 "gpt-4-32k",
+                                 "gpt-4-32k-0314",
+                                 "gpt-4-32k-0613",
+                                 "gpt-3.5-turbo",
+                                 "gpt-3.5-turbo-16k",
+                                 "gpt-3.5-turbo-0301",
+                                 "gpt-3.5-turbo-0613",
+                                 "gpt-3.5-turbo-1106",
+                                 "gpt-3.5-turbo-0125",
+                                 "gpt-3.5-turbo-16k-0613"]
+        return openai_api_models
 
     @staticmethod
     def validate_messages(messages: list):
@@ -400,43 +502,3 @@ class TestClient:
                 print(f"The error: {error_message}")
                 print(f"The messages: {messages}")
                 time.sleep(2)
-
-
-if __name__ == "__main__":
-    test_client = TestClient(api_key="sk-1", top_p=0.8911, n=2, logit_bias={"s": 0})
-    test_client.validate_arguments()
-    mock_messages = [{"role": "assistant", "content": "yes", "name": "anc"}]
-    mock_tools = [
-        {
-            "type": "function",
-            "function": {
-                "name": "get_current_weather",
-                "description": "Get the current weather in a given location",
-                "parameters": {
-                    "type": "object",
-                    "properties": {
-                        "location": {
-                            "type": "string",
-                            "description": "The city and state, e.g. San Francisco, CA"
-                        },
-                        "unit": {
-                            "type": "string",
-                            "enum": ["celsius", "fahrenheit"]
-                        }
-                    },
-                    "required": ["location"]
-                }
-            }
-        }
-    ]
-
-    normal_response = test_client.run(messages=mock_messages, tools=mock_tools)
-    print(normal_response)
-
-    stream_response = test_client.stream_run(messages=mock_messages)
-    import time
-
-    for i in stream_response:
-        print(i, end='', flush=True)
-        time.sleep(0.1)
-    print()
